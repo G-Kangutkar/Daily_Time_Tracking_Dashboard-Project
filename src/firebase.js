3d2e1c43a0ef4f71f9480515f3622adff5b422f7
G-Kangutkar <139991096+G-Kangutkar@users.noreply.github.com>
2025-12-09 16:29:25 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,22 +3,14 @@ import { getAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAVEEtsi39DBChHj_Cu2V04gzulVeZDOcs",
-  authDomain: "ai-project-36184.firebaseapp.com",
-  databaseURL: "https://ai-project-36184-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "ai-project-36184",
-  storageBucket: "ai-project-36184.firebasestorage.app",
-  messagingSenderId: "1040023061480",
-  appId: "1:1040023061480:web:25d0c9e23171cde3999a5d",
-  measurementId: "G-LQYGZ1DTCD"
-};
-const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL, 
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID, 
 };
 
 const app = initializeApp(firebaseConfig);
